133a6976ebc6092c6c9528c2750ea29e8fdd8d01
alisabouri <alisabouri6857@gmail.com>
2024-12-28 23:23:59 +0330

    end animation js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,15 @@
+
+const emoji =document.querySelector('.parent-emoji')
 window.addEventListener('load',()=>{
 const animateTittle = document.querySelectorAll('.parent-span')
 animateTittle.forEach(items=>{
     items.classList.add('animate-tittle-1')
        
     })
-    document.querySelector('.parent-emoji').classList.add('animate-tittle-2')
+    emoji.classList.add('animate-tittle-2')
     setTimeout(() => {
-        document.querySelector('HEADER').style.height = "300px"
+        document.querySelector('HEADER').style.maxHeight = "200px"
+        emoji.classList.add ("emojiend")
         
     }, 3000);
     document.addEventListener('mousemove' , e=>{
